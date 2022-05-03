9287da64c5da7e79e745318ab24a3b0120bdb493
GeorgeYin2004 <99358543+GeorgeYin2004@users.noreply.github.com>
2022-05-03 14:02:16 -0400

    Add files via upload
    work in progress

---
+++ ShoppingCart.java
@@ -0,0 +1,19 @@
+/** Name: George Yin
+  * Date: May 1 2022
+  * Description: This program contains the Shopping Cart class.
+  */
+import java.util.*;
+
+public class ShoppingCart {
+
+   /* Attributes */
+   
+   private int numberOfItems;
+   
+   private double discount;
+   
+   private int avaliableNumber;
+   
+   
+   
+   
