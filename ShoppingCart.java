b2064bbbb543615522d7b9bb2850495a9164049b
GeorgeYin2004 <99358543+GeorgeYin2004@users.noreply.github.com>
2022-05-04 12:17:15 -0400

    Add files via upload

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -5,6 +5,9 @@
 import java.util.*;
 
 public class ShoppingCart {
+   public ShoppingCart() {
+      this.numberOfItems = 0;
+   }
 
    /* Attributes */
    
@@ -28,7 +31,23 @@ public class ShoppingCart {
    
    /* Accesors */
    
+   /* getters */
    
+   public int getNumberOfItems() {
+      return this.numberOfItems;
+   }
    
+   /* setters */
    
+   public void set
    
+   /**Method for returning the important data
+     *@return the important data of the human
+     */
+   public String toString(){
+     String shoppingCartData = "Number Of item(s) in shopping cart: " + this.numberOfItems +  
+         return shoppingCartData;
+   }
+    
+  }
+}
