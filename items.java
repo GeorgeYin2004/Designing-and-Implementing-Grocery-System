b1cb7cab4fd77637295ad69fe00658c35e9eec5a
Hooman100 <100724958+Hooman100@users.noreply.github.com>
2022-05-04 11:23:08 -0400

    Add files via upload

---
+++ items.java
@@ -0,0 +1,27 @@
+/** Name: Hooman Moradian-Nezhad
+  * Date: May 4 2022
+  * Description: This program contains the items class.
+  */
+import java.util.*;
+
+public class items {
+
+   /* Attributes */
+   
+   /** Identification of product */
+   private int productid;
+   
+   
+   /** Stock of the product */
+   private int avaliableNumber;
+   
+   /** Gets item name */
+   private String productname;
+   
+   /** Cost of the item */
+   private double productprice;
+   
+  
+   
+   
+   /* Accesors */
