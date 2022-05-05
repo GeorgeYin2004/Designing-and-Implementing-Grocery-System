5aa21d263cb42a0f202198b72291f8bbd6c1e352
RyanX740 <99358718+RyanX740@users.noreply.github.com>
2022-05-05 14:05:22 -0400

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,16 @@
+/**
+ * Designer: Ryan Xiong
+ * Date: May 4
+ * Description: Product class
+ */
+public class Product {
+    private String productName;
+
+    public String getProductName(){
+        return productName;
+    }
+
+    public void setProductName(String productName){
+        this.productName = productName;
+    }
+}
