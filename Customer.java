b3efeb7875108918316471cf1c7844c19ccf63f1
RyanX740 <99358718+RyanX740@users.noreply.github.com>
2022-05-12 12:34:36 -0400

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -5,18 +5,99 @@
  */
 public class Customer {
     private String customerName;
+    private int customerId;
+    private boolean gender;
+    private String genderStr;
 
+   // public Customer(String customerName, int customerId, boolean gender){
+        //this.customerName = customerName;
+        //this.customerId = customerId;
+        //this.gender = gender;
+    //}
+    //Getter
+    /**
+     * @return The name of customer
+     */
     public String getName(){
 
         return customerName;
     }
 
+    /**
+     * @return The ID of customer
+     */
+    public int getCustomerId(){
+
+        return customerId;
+    }
+
+    /**
+     * if gender = true, the customer is a male; if gender = false, the customer is a female
+     * @return The gender of customer
+     */
+    public boolean getGender(){
+
+        if(gender==true){
+            genderStr = "male";
+        }else{
+            genderStr = "female";
+        }
+        return gender;
+
+    }
+
+    //Setter
+
+    /**
+     * @param customerName The name of customer
+     */
     public void setName(String customerName){
 
         this.customerName = customerName;
     }
-    Product shopping(Market market, String productName){
 
-        return market.sell(productName);
+    /**
+     * @param customerId The ID of customer
+     */
+    public void setCustomerId(int customerId){
+
+        this.customerId = customerId;
+    }
+
+    /**
+     * @param gender The gender of customer
+     */
+    public void setGender(boolean gender){
+
+        this.gender = gender;
+        if(gender==true){
+            genderStr = "male";
+        }else{
+            genderStr = "female";
+        }
+    }
+
+    /**
+     * Update the customer information
+     * @param customerName The name of customer
+     * @param customerId The Id of customer
+     * @param gender The gender of customer
+     */
+    public void updateProfile(String customerName, int customerId, boolean gender){
+        this.customerName = customerName;
+        this.customerId = customerId;
+        this.gender = gender;
+    }
+    
+    /**
+     * @return The information of customer
+     */
+    public String toString(){
+        if(gender==true){
+            genderStr = "male";
+        }else{
+            genderStr = "female";
+        }
+        return "Customer name: " + customerName + ", Customer ID: " + customerId + ", Gender: " + genderStr;
     }
 }
