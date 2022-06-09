0ecb54e65a57f866050f1fb5858f1a5c6d27ac86
Hooman100 <100724958+Hooman100@users.noreply.github.com>
2022-06-08 22:46:12 -0400

    Add files via upload

--- shipper.java
+++ shipper.java
@@ -1,72 +1,19 @@
-/** Name: Hooman Moradian-Nezhad
-  * Date: May 4 2022
-  * Description: This program contains the items class.
-  */
-import java.util.*;
-import java.util.Scanner;
+class shipper extends employee {
+  private String ShipperName = "Bob";    //shipper attributes
+  private int Shipperid = 560;
+  private String Hiredate = "6/2/2022";
+  private int workdays = 5;
+  private double salaryperweek = 500.0;
+  public static void main(String[] args) {
 
+    // Create a myCar object
+    Car myemployee = new employee();
 
-public class items {
-
-   /* Attributes */
-   
-   /** Identification of product */
-   private int productid;
-   
-   
-   /** Stock of the product */
-   private int avaliableNumber;
-   
-   /** Gets item name */
-   private String productname;
-   
-   /** Cost of the item */
-   private double productprice;
-   
-  
-   
-   
-   /* Accesors */
-   
-    /* getters */
-    public class GroceryItemOrder
-
-{
-
-    public static void main (String [] args)
-
-        {
-            int totalcost = 0;
-
-        //Initializes scanner for user input
-
-        Scanner in = new Scanner (System.in);
-
- 
-
-    // Constructs an item order to purchase the item with the given name, in the given quantity, which costs the given price per unit.
-
-    public GroceryItemOrder(String name)
-
-    {
-        System.out.println ("Enter the grocery item that you want:");
-
-        String name.nextLine ();
-  
-   public int getItem() {
-      return this.ItemName;
-   }
-   
-   /* setters */
-   
-   public void set
-   
-   /* choosing item
-     */
-   public String toString(){
-     String shoppingCartData = "Name of Item: " + this.ItemName +  
-         return Itemchosen;
-   }
+    // Call the honk() method (from the Vehicle class) on the myCar object
+    myemployee.shipsitems();
     
+
+    // Display the value of the brand attribute (from the Vehicle class) and the value of the modelName from the Car class
+    System.out.println(myEmployee.type + " " + myEmployee.shipperName);
   }
 }
